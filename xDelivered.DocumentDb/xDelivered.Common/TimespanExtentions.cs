9a714c447559a84f753516cbc3bf3469bd8ad906
Patrick <patrick@xdelivered.com>
2017-03-07 00:10:33 +0000

    sdasdsa

---
+++ xDelivered.DocumentDb/xDelivered.Common/TimespanExtentions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace xDelivered.Common
+{
+    public static class TimespanExtentions
+    {
+        public static string ToIsoFormat(this TimeSpan source)
+        {
+            return XmlConvert.ToString(source);
+        }
+    }
+}
